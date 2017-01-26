dccbf48309cbf2259ed01a89ddf9984edcefa5d1
Le-Yo <le-yo@le-yo.com>
2017-01-26 21:41:28 +0300

    user input and conversation script

---
+++ conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo Hello, who am I talking to?
+read varname
+echo It\'s nice to meet you $varname
+echo How can I help you?
+read varname2
+echo Sorry I don\'t have an answer for that
